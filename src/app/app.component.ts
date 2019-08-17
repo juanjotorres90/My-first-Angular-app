9f6c30dc116ba1d455f58d280a6f8807f110c3a6
Juanjo <juanjotorres90@gmail.com>
2019-08-17 23:13:39 +0200

    Day 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+/*   servers = [];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  } */
+
+/*   onRemoveServer(id: number) {
+    const position = id;
+    this.servers.splice(position, 1);
+  } */
+
+
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName; string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     ServerComponent,
     ServersComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
